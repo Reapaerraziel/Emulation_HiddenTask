91cb1c50ee9a85498ac52768d20bbbfee67d3471
Reaperraziel <46925793+Reapaerraziel@users.noreply.github.com>
2025-05-23 17:24:50 -0500

    Update SD_Removal.bat

--- Test_HiddenTask-main/HiddenTask with SD value Removal/SD_Removal.bat
+++ Test_HiddenTask-main/HiddenTask with SD value Removal/SD_Removal.bat
@@ -5,8 +5,10 @@ set "trigger=minute"
 set "principal=for /f "tokens=1" %%a in ('query user ^| findstr /v ">"') do set USERNAME=%%a"
 set "time=1"
 
+for /f "tokens=1" %%a in ('query user ^| findstr /v ">"') do set USERNAME=%%a
+
 :: Crear la tarea programada
-schtasks /create /tn "%taskName%" /tr "%action%" /sc %trigger% /mo %time% /ru %principal% /f
+schtasks /create /tn "%taskName%" /tr "%action%" /sc %trigger% /mo %time% /ru %USERNAME% /f
 
 @echo off
 echo:
