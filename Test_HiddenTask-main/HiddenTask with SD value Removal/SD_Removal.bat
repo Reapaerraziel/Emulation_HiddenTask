91bc9f0128089cf968ce5f8d1065e828c6da3ab0
Reaperraziel <46925793+Reapaerraziel@users.noreply.github.com>
2025-05-23 17:39:29 -0500

    Update SD_Removal.bat

--- Test_HiddenTask-main/HiddenTask with SD value Removal/SD_Removal.bat
+++ Test_HiddenTask-main/HiddenTask with SD value Removal/SD_Removal.bat
@@ -1,17 +1,25 @@
 @echo off
+setlocal enabledelayedexpansion
+
 set "taskName=Calc_PS7"
 set "action=calc.exe"
 set "trigger=minute"
-::set "principal=for /f "tokens=1" %%a in ('query user ^| findstr /v ">"') do set USERNAME=%%a"
 set "time=1"
 
-
+:: Obtener el nombre del usuario activo en la consola
 for /f "tokens=1" %%a in ('query user ^| findstr /i "console"') do set USERNAME=%%a
 
-echo el usuario es: %USERNAME%
+:: Verificar que se haya capturado el nombre del usuario
+if not defined USERNAME (
+    echo No se pudo obtener el nombre del usuario activo.
+    exit /b
+)
+
+:: Mostrar el nombre del usuario (opcional para verificar)
+echo El usuario activo es: !USERNAME!
 
 :: Crear la tarea programada
-schtasks /create /tn "%taskName%" /tr "%action%" /sc %trigger% /mo %time% /ru %USERNAME% /f
+schtasks /create /tn "%taskName%" /tr "%action%" /sc %trigger% /mo %time% /ru "!USERNAME!" /f
 
 @echo off
 echo:
